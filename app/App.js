cd72b0bf806d51b3204f4b7af626981837255581
Dima Beznos <beznos.d@gmail.com>
2017-05-19 23:33:17 +0300

    Tried highlight

---
+++ app/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import Code from 'Code';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // code: 'var days = 31;'
+      code: ''
+    };
+
+    this.handleChooseFile = this.handleChooseFile.bind(this);
+    this.handleLoadFile = this.handleLoadFile.bind(this);
+  }
+
+  handleChooseFile() {
+    this.fileInput.click();
+  }
+
+  handleLoadFile() {
+    const file = this.fileInput.files[0];
+    const fileReader = new FileReader();
+    
+    fileReader.readAsText(file);
+
+    fileReader.onload = () => {
+      // console.log(fileReader.result);
+      for (let i = 0; i < fileReader.result.length; i++) {
+        // console.log(fileReader.result[i] + ' => ' + fileReader.result[i].charCodeAt(0));
+      }
+      this.setState({ code: fileReader.result });
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form style={{textAlign: 'center'}}>
+          {/*<button onClick={this.handleChooseFile}>LOAD FILE</button>*/}
+          <input 
+            ref={element => this.fileInput = element} 
+            onChange={this.handleLoadFile}
+            type="file"
+            name="file"
+            className="file"
+            id="file"
+            hidden
+          />
+          <label className="loadFileButton" htmlFor="file">LOAD FILE</label>
+        </form>
+        <br/>
+        <Code code={this.state.code} />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ brunch-config.js
@@ -0,0 +1,7 @@
+// See http://brunch.io for documentation.
+module.exports = {
+  files: {
+    javascripts: {joinTo: 'app.js'},
+    stylesheets: {joinTo: 'app.css'}
+  }
+};
