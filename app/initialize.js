1e319d6406ec8c8aef349f1099d2f1016a019ac2
Dima Beznos <beznos.d@gmail.com>
2017-06-06 19:10:05 +0300

    First production build

--- app/initialize.js
+++
@@ -1,5 +0,0 @@
-// const Prism = require('prismjs');
-// var code = 'var a = 10;';
-// var html = Prism.highlight(code, Prism.languages.javascript);
-// document.getElementById('code').innerHTML = html;
-
